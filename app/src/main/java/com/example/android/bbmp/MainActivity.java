34c891f2f43561cc117037801aa3a0fdd2b1e145
riatauro <tauroelevita@gmail.com>
2015-10-19 18:05:43 +0530

    jfhgrfk

---
+++ app/src/main/java/com/example/android/bbmp/MainActivity.java
@@ -0,0 +1,215 @@
+package com.example.android.bbmp;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.wallet.Address;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    public final static String EXTRA_MESSAGE="com.example.myfirstapp.MESSAGE";
+   public static String item;
+
+  //  final EditText editText = (EditText) findViewById(R.id.edit);
+   //public static String address
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    /*    Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.issues_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
+        Spinner spinnere = (Spinner) findViewById(R.id.spinner1);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.location_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinnere.setAdapter(adapter1);
+
+
+
+    }
+      /*  spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                pos=i;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        } {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                pos=i;
+            }
+        });
+        spinnere.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                pos1=i;
+            }
+        });
+    }*/
+
+
+
+  /*public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+      pos1=parent.getItemAtPosition(pos).toString();
+
+
+
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+*/
+
+
+
+        Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+
+        // Spinner click listener
+        spinner1.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> categories1 = new ArrayList<String>();
+        categories1.add("Potholes");
+        categories1.add("Garbage Crisis");
+        categories1.add("Dying Lakes");
+        categories1.add("Depleting Greenery");
+        categories1.add("Street Lights");
+
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories1);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner1.setAdapter(dataAdapter1);
+
+
+    // Spinner element
+    Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+    // Spinner click listener
+    spinner.setOnItemSelectedListener(this);
+
+    // Spinner Drop down elements
+    List<String> categories = new ArrayList<String>();
+    categories.add("Yelahanka");
+    categories.add("Jayanagar");
+    categories.add("Dasarahalli");
+    categories.add("Rajarajeshwarinagar");
+    categories.add("Hebbal");
+    categories.add("K.R.Puram");
+        categories.add("Yeshwanthpur");
+        categories.add("C.V.Ramanagar");
+        categories.add("Shivajinagar");
+        categories.add("Rajajinagar");
+        categories.add("Vijayanagar");
+        categories.add("Chamrajpet");
+        categories.add("Chikpet");
+        categories.add("BTM Layout");
+        categories.add("Basavangudi");
+
+
+    // Creating adapter for spinner
+    ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+    // Drop down layout style - list view with radio button
+    dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+    // attaching data adapter to spinner
+    spinner.setAdapter(dataAdapter);
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+
+        if(parent.getId()==R.id.spinner) {
+            item = parent.getItemAtPosition(position).toString();
+        }
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+
+    }
+
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+
+    public void sendMessage(View view){
+
+        Intent intent=new Intent(this,MapsActivity.class);
+
+        //intent.putExtra("pos2",pos1);
+
+        startActivity(intent);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/android/bbmp/MapsActivity.java
@@ -0,0 +1,189 @@
+package com.example.android.bbmp;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.graphics.Point;
+
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatCallback;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.Projection;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+
+public class MapsActivity extends AppCompatActivity {
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    Toolbar toolbar;
+    ImageButton FAB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Intent intent = getIntent();
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+       toolbar = (Toolbar)  findViewById(R.id.tool_bar);
+
+       setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //Log.d("","");
+        FAB = (ImageButton) findViewById(R.id.imageButton);
+        FAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                }
+        });
+
+        setUpMapIfNeeded();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        LatLng p =new LatLng(0,0);
+               //new LatLng(12.96,77.86);
+
+
+       switch(MainActivity.item)
+        {
+            case "Yelahanka":p=new LatLng(13.1139, 77.5983);
+                                 break;
+            case "Jayanagar":p=new LatLng(12.9250, 77.5950);
+                              break;
+            case "Dasarahalli":p=new LatLng(13.0987, 77.8389);
+                               break;
+            case "Rajarajeshwarinagar":p=new LatLng(12.9242199,77.5191194);
+                break;
+            case "Hebbal":p=new LatLng(13.0400, 77.5900);
+                break;
+            case "K.R.Puram":p=new LatLng(12.9950, 77.6800);
+                break;
+            case "Yeshwanthpur":p=new LatLng(13.0285, 77.5462);
+                break;
+            case "C.V.Ramanagar":p=new LatLng(12.9856, 77.6639);
+                break;
+            case "Shivajinagar":p=new LatLng(12.9900, 77.6000);
+                break;
+            case "Rajajinagar":p=new LatLng(12.9700, 77.5700);
+                break;
+            case "Vijayanagar":p=new LatLng(12.9600, 77.5400);
+                break;
+            case "Chamrajpet":p=new LatLng(12.9500, 77.5500);
+                break;
+            case "Chikpet":p=new LatLng(12.9719, 77.5772);
+                break;
+            case "BTM Layout":p=new LatLng(12.9200, 77.6100);
+                break;
+            case "Basavangudi":p=new LatLng(12.9400, 77.5700);
+                break;
+
+
+        }
+      //  Toast.makeText(getApplicationContext(), "" + MainActivity.pos1, Toast.LENGTH_LONG).show();
+       mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(p,13));
+        mMap.addMarker(new MarkerOptions().position(p).title("Marker")).setDraggable(true);
+
+
+        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                LatLng pos = marker.getPosition();
+                Geocoder geocoder;
+                List<Address> addresses=null;
+                geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+              try {
+                  addresses = geocoder.getFromLocation(pos.latitude, pos.longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+              }
+              catch(IOException e)
+              {
+                  e.printStackTrace();
+              }
+                String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                String city =    addresses.get(0).getLocality();
+                String state = addresses.get(0).getAdminArea();
+                String country = addresses.get(0).getCountryName();
+                String postalCode = addresses.get(0).getPostalCode();
+                String knownName = addresses.get(0).getFeatureName();
+                Toast.makeText(getApplicationContext(), "" + address + city , Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+    }
+
+    }
